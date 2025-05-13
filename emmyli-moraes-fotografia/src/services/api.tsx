f5aeadc094df3be06c4a9c16c2970c48fe204b39
Adriel-Agnes <adriel.agnes@gmail.com>
2025-05-12 23:27:42 -0300

    adição do /api

--- emmyli-moraes-fotografia/src/services/api.tsx
+++ emmyli-moraes-fotografia/src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // const url = 'http://localhost:3000'
-const url = 'https://emmylifotografias.com.br'
+const url = 'https://emmylifotografias.com.br/api'
 
 export const api = axios.create ({baseURL: url});
