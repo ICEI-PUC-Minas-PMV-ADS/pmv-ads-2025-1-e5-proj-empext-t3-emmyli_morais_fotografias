d3278d75a33c5c1c07a72bb82918b41bef6b8d7a
Adriel-Agnes <adriel.agnes@gmail.com>
2025-06-10 01:52:37 -0300

    Adição de numero de telefone a tabela Usuarios

--- codigo-fonte/api/src/models/Usuarios.js
+++ codigo-fonte/api/src/models/Usuarios.js
@@ -1,58 +1,61 @@
 module.exports = (sequelize, DataTypes) => {
-    const Usuarios = sequelize.define('Usuarios', {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      nome: {
-        type: DataTypes.STRING(100),
-        allowNull: false
-      },
-      email: {
-        type: DataTypes.STRING(150),
-        allowNull: false,
-        unique: true
-      },
-      login: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        unique: true
-      },
-      senha_hash: {
-        type: DataTypes.TEXT,
-        allowNull: false
-      },
-      tipo: {
-        type: DataTypes.ENUM('fotografo', 'cliente'),
-        allowNull: false
-      },
-      dtinclusao: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
-      },
-      dtalteracao: {
-        type: DataTypes.DATE
-      }
-    }, {
-      tableName: 'usuarios',
-      timestamps: false
+  const Usuarios = sequelize.define('Usuarios', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    nome: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      unique: true
+    },
+    login: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true
+    },
+    senha_hash: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    tipo: {
+      type: DataTypes.ENUM('fotografo', 'cliente'),
+      allowNull: false
+    },
+    telefone: {
+      type: DataTypes.STRING(20),
+      allowNull: true,
+    },
+    dtinclusao: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    dtalteracao: {
+      type: DataTypes.DATE
+    }
+  }, {
+    tableName: 'usuarios',
+    timestamps: false
+  });
+
+  Usuarios.associate = (models) => {
+    Usuarios.hasOne(models.DetalheUsuario, {
+      foreignKey: 'idusuario',
+      as: 'detalhes',
+      onDelete: 'CASCADE'
     });
-  
-    Usuarios.associate = (models) => {
-      Usuarios.hasOne(models.DetalheUsuario, {
-        foreignKey: 'idusuario',
-        as: 'detalhes',
-        onDelete: 'CASCADE'
-      });
 
-      Usuarios.hasMany(models.Comentarios, {
-        foreignKey: 'usuarioId',
-        as: 'comentarios'
-      });
+    Usuarios.hasMany(models.Comentarios, {
+      foreignKey: 'usuarioId',
+      as: 'comentarios'
+    });
 
   };
-  
-    return Usuarios;
-  };
-  
+
+  return Usuarios;
+};
